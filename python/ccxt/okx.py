a1009e2e8776270c28f93799e6d33d549c8fbd73
timertimertimer <timerkhan2002@gmail.com>
2025-05-29 16:36:22 +0300

    add deposit limits to okx

--- python/ccxt/okx.py
+++ python/ccxt/okx.py
@@ -1866,6 +1866,10 @@ class okx(Exchange, ImplicitAPI):
                             'min': self.safe_number(chain, 'minWd'),
                             'max': self.safe_number(chain, 'maxWd'),
                         },
+                        'deposit': {
+                            'min': self.safe_number(chain, 'minDep'),
+                            'min': self.safe_number(chain, 'maxDep'),
+                        }
                     },
                     'info': chain,
                 }
